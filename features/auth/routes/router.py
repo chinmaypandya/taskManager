09c8e3e3a7b8f8fe96df72780cd4b4267040f654
Chinmay Pandya <chinmaypandya043@gmail.com>
2024-08-20 07:02:30 +0530

    style(router): formatted auth router code

--- features/auth/routes/router.py
+++ features/auth/routes/router.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Request, Response, Cookie
+from fastapi import APIRouter, Request, Cookie
 from features.auth.models.user import User, loginUser
 from features.auth.operations.operations import login, logout, signup, send_session
 from middlewares.limiter import limiter
@@ -8,15 +8,15 @@ auth_router = APIRouter()
 
 @auth_router.post('/auth/login')
 @limiter.limit('2/second')
-async def login_user(user: loginUser, request:Request, response: Response):
+async def login_user(user: loginUser, request: Request):
     return login(dict(user))
 
 @auth_router.post('/auth/signup')
 @limiter.limit('2/second')
-async def signup_user(user: User,request:Request, response: Response):
+async def signup_user(user: User, request: Request):
     return signup(dict(user))
 
-@auth_router.get('/auth/logout')
+@auth_router.delete('/auth/logout')
 @limiter.limit('2/second')
 async def logout_user(request: Request):
     return logout()
